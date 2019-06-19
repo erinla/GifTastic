eb9d018033962eb98a3644e914741fce27c36432
erinla <erin_lawler@yahoo.com>
2019-06-18 19:02:45 -0500

    works but needs styling

--- app.js
+++ app.js
@@ -1,66 +1,69 @@
-console.log("js is linked");
+$(document).ready(function() {
+  var tvShows = ['new girl', 'the office'];
 
-
-$(document).ready(function () {
-    var tvShows = [];
-
-    function addButtons() {
-        $("#buttons-appear-here").empty();
-        for (var i = 0; i < tvShows.length; i++) {
-            var addButton = $("<button>");
-            addButton.addClass("tv-show");
-            // a.attr("data-name", movies[i]);
-            addButton.text(tvShows[i]);
-            $("#buttons-appear-here").append(addButton);
-        }
-
+  function addButtons() {
+    $('#buttons-appear-here').empty();
+    for (var i = 0; i < tvShows.length; i++) {
+      var addButton = $('<button>');
+      addButton.addClass('tv-show');
+      addButton.attr('data-program', tvShows[i]);
+      addButton.text(tvShows[i]);
+      $('#buttons-appear-here').append(addButton);
     }
+  }
 
-    $("#submit").on("click", function (event) {
-        event.preventDefault();
+  addButtons();
 
-        // This line will grab the text from the input box
-        var tvShow = $("#user-input").val().trim();
-        console.log(tvShow);
-        tvShows.push(tvShow);
-        console.log(tvShows);
+  $('#submit').on('click', function(event) {
+    event.preventDefault();
+    var tvShow = $('#user-input')
+      .val()
+      .trim();
+    tvShows.push(tvShow);
+    addButtons();
+  });
 
-        addButtons();
+  $(document).on('click', '.tv-show', function() {
+    $('#gifs-appear-here').empty();
+    var program = $(this).attr('data-program');
+    var queryURL =
+      'https://api.giphy.com/v1/gifs/search?q=' +
+      program +
+      '&api_key=ZJtZOUSM26x0m1plnw3Ns5FjnDHH23I4';
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).then(function(response) {
+      var results = response.data;
+      for (var i = 0; i < 5; i++) {
+        var gifDiv = $('<div>');
+        var rating = results[i].rating;
+        var p = $('<p>').text('Rating: ' + rating);
+        var personImage = $('<img>');
+        personImage.addClass('gifImage');
+        personImage.attr('src', results[i].images.fixed_height_still.url);
+        personImage.attr(
+          'data-still',
+          results[i].images.fixed_height_still.url
+        );
+        personImage.attr('data-animate', results[i].images.fixed_height.url);
+        personImage.attr('data-state', 'still');
+        gifDiv.prepend(p);
+        gifDiv.prepend(personImage);
+
+        $('#gifs-appear-here').prepend(gifDiv);
+      }
     });
+  });
 
-
-
-    // $("button").on("click", function () {
-    //     var person = $(this).attr("data-person");
-    //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    //         person + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-
-    //     $.ajax({
-    //         url: queryURL,
-    //         method: "GET",
-
-    //     })
-    //         .then(function (response) {
-    //             var results = response.data;
-    //             console.log(results)
-    //             console.log(response)
-
-    //             for (var i = 0; i < results.length; i++) {
-    //                 var gifDiv = $("<div>");
-
-    //                 var rating = results[i].rating;
-
-    //                 var p = $("<p>").text("Rating: " + rating);
-    //                 console.log(results[i])
-    //                 var personImage = $("<img>");
-    //                 personImage.attr("src", results[i].images.fixed_height.url);
-
-    //                 gifDiv.prepend(p);
-    //                 gifDiv.prepend(personImage);
-
-    //                 $("#gifs-appear-here").prepend(gifDiv);
-    //             }
-    //         });
-    // });
-
+  $(document).on('click', '.gifImage', function() {
+    var state = $(this).attr('data-state');
+    if (state === 'still') {
+      $(this).attr('src', $(this).attr('data-animate'));
+      $(this).attr('data-state', 'animate');
+    } else {
+      $(this).attr('src', $(this).attr('data-still'));
+      $(this).attr('data-state', 'still');
+    }
+  });
 });
